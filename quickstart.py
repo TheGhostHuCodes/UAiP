35e72bcc629708361e3f09db76b2bae34f3e0ce2
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2020-08-17 22:17:54 -0700

    Add asyncio quickstart program

---
+++ quickstart.py
@@ -0,0 +1,11 @@
+import asyncio, time
+
+
+async def main():
+    print(f"{time.ctime()} Hello!")
+    await asyncio.sleep(1.0)
+    print(f"{time.ctime()} Goodbye!")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
