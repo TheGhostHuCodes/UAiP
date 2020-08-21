34dc4e22486df12f2ddaf83685d2b117830f8658
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2020-08-20 20:41:12 -0700

    Fix the bug in quickstart by bringing your own loop and executor

--- quickstart.py
+++ quickstart.py
@@ -3,29 +3,34 @@ from concurrent.futures import ThreadPoolExecutor as Executor
 import time
 
 
-async def make_coro(future):
-    try:
-        return await future
-    except asyncio.CancelledError:
-        return await future
-
-
 async def main():
-    loop = asyncio.get_running_loop()
-    future = loop.run_in_executor(None, blocking)
-    asyncio.create_task(make_coro(future))
     print(f"{time.ctime()} Hello!")
     await asyncio.sleep(1.0)
     print(f"{time.ctime()} Goodbye!")
+    loop.stop()
 
 
 def blocking():
-    time.sleep(1.5)
+    time.sleep(2.0)
     print(f"{time.ctime()} Hello from a thread!")
 
 
 if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    executor = Executor()
+    loop.set_default_executor(executor)
+    loop.create_task(main())
+    future = loop.run_in_executor(None, blocking)
     try:
-        asyncio.run(main())
+        loop.run_forever()
     except KeyboardInterrupt:
-        print("Bye!")
+        print("Cancelled")
+
+    tasks = asyncio.all_tasks(loop=loop)
+    for t in tasks:
+        t.cancel()
+
+    group = asyncio.gather(*tasks, return_exceptions=True)
+    loop.run_until_complete(group)
+    executor.shutdown(wait=True)
+    loop.close()
